1e3ef6089c27f31c9c1f35baf9403cedfcdc7b89
rajat <rajat@kali.rpaw.org>
2021-05-22 17:38:29 +0530

    Dismissing keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Alert, FlatList, StyleSheet, Text, View} from 'react-native';
+import {Alert, FlatList, Keyboard, StyleSheet, TouchableWithoutFeedback, View} from 'react-native';
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo"
@@ -25,21 +25,26 @@ export default function App() {
     })}
   }
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        {/*to form*/}
-        <AddTodo submitHandler={submitHandler}/>
-        <View style={styles.list}>
-          <FlatList
-              data={todos}
-              renderItem={({item})=>(
-              <TodoItem item={item} pressHandler={pressHandler}/>
-              )}
-          />
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+      console.log('Dismissing keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          {/*to form*/}
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+                data={todos}
+                renderItem={({item})=>(
+                <TodoItem item={item} pressHandler={pressHandler}/>
+                )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
